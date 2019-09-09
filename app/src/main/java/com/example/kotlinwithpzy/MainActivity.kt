1c7e4a985ed0d0e1cbbce0d97f6c8fd54a495f9a
Darwin Santana <darwi1983@gmail.com>
2019-09-09 10:02:50 -0400

    add some funtions

--- app/src/main/java/com/example/kotlinwithpzy/MainActivity.kt
+++ app/src/main/java/com/example/kotlinwithpzy/MainActivity.kt
@@ -2,18 +2,50 @@ package com.example.kotlinwithpzy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
+
+
+// clase implicita de datos(Data class)
+data class Cursos(val nombre: String, val url:String)
 
 class MainActivity : AppCompatActivity() {
 
-    // clase implicita de datos(Data class)
-     data class Cursos
+    val react =  Cursos("react","react")
+    val kot = Cursos("Kotlin","kotlin")
+    var cursoActual=react.copy()
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val button: Button = findViewById(R.id.botoncito)
+        button.setOnClickListener {
+            view ->  switchCursor(cursoActual)
+        }
+        //verEnPantalla("cursos de ${cursoActual.nombre}  en platzi.com: ${cursoActual.url}")
+    }
+
+    fun switchCursor(curso : Cursos){
+         cursoActual = curso.copy()
+
+        when(curso.url){
+            "react" -> cursoActual= kot.copy()
+            "kotlin" -> cursoActual = react.copy()
+            else -> println("esto es imposible")
+        }
+
+
+        verEnPantalla("cursos de ${cursoActual.nombre}  en platzi.com: ${cursoActual.url}")
+
+    }
+
+    fun verEnPantalla(s : String){
         val txt: TextView= findViewById(R.id.mensaje)
-        txt.setText("si funciona")
+        txt.setText(s)
+
     }
 }
